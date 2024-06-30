15f7142bc691c52207073601063518953f710e0c
subodhdhital <subodhdhital79@gmail.com>
2024-06-30 10:04:33 +0545

    minors

--- javascript/animation.js
+++ javascript/animation.js
@@ -4,7 +4,7 @@ gsap.to("#home .herocontent h1",{
     scrollTrigger:{
         trigger:"#home .herocontent h1",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top 20%",
         end:"top 5%",
         scrub:3,
@@ -17,7 +17,7 @@ gsap.to("#home .herocontent p",{
     scrollTrigger:{
         trigger:"#home .herocontent p",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top 30%",
         end:"top 15%",
         scrub:3
@@ -29,20 +29,21 @@ gsap.to("#home .herocontent .hero-btn",{
     scrollTrigger:{
         trigger:"#home .herocontent .hero-btn",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top 65%",
         end:"top 50%",
         scrub:2,
     }
 })
 gsap.to("#about p",{
-    transform:"translateX(-680%)",
+    transform:"translateX(-150%)",
     ease: "slow(0.7,0.7,false)",
     scrollTrigger:{
         trigger:"#about",
         scroller:"body",
         // markers:true,
         start:"top 0%",
+        end:"top -150%",
         scrub:2,
         pin:true
     }
