71f9d524fb25679f8c5b89f48a2aa632ffb059ec
subodhdhital <subodhdhital79@gmail.com>
2024-07-02 22:58:39 +0545

    some major changes

---
+++ javascript/menu.js
@@ -0,0 +1,25 @@
+var menu = document.querySelector(".nav .hamburger i")
+var cross = document.querySelector(".menu i")
+var t = gsap.timeline()
+
+t.to(".menu",{
+    right:0,
+    duration:0.5,
+
+})
+t.from(".menu .men",{
+    x:150,
+    duration:0.4,
+    stagger:0.3,
+    opacity:0
+})
+t.from(".menu i",{
+    opacity:0
+})
+t.pause()
+menu.addEventListener("click",function(){
+    t.play()
+})
+cross.addEventListener("click",function(){
+    t.reverse()
+})
